685d90c7017865b447d888b4e436ef48a3f58c17
yangxdev <j.xiang@campus.unimib.it>
2023-03-09 10:36:49 +0100

    workData.json

--- src/js/work.js
+++ src/js/work.js
@@ -1,5 +1,6 @@
 import WorkItem from "./workitem.js";
 import { List } from "semantic-ui-react";
+import workData from '../json/workData.json';
 
 function Work() {
   return (
@@ -7,58 +8,26 @@ function Work() {
       <div className="title-work-experience">WORK EXPERIENCE</div>
       <hr />
       <div className="work-experience-content">
-        <WorkItem
-          title="Analyst & Full Stack Developer"
-          company="Deloitte"
-          link="https://www2.deloitte.com/it/it.html"
-          date="March 2023 - Present"
-          locationlink="https://www.google.com/maps/place/Milan,+Italy"
-          location="Milan, MI"
-          description={
-            <div className="justify">
-              <List bulleted>
-                <List.Item>Starting as an Analyst with Full Stack Developer title.</List.Item>
-              </List>
-            </div>
-          }
-        />
-        <WorkItem
-          title="Room Service Waiter"
-          company="Bento Sushi"
-          link="https://goo.gl/maps/gzsfJ9QELKVYoVqD6"
-          date="June 2019 - September 2020"
-          locationlink="https://www.google.com/maps/place/Cattolica,+Italy"
-          location="Cattolica, RN"
-          description={
-            <div className="justify">
-              <List bulleted>
-                <List.Item>Taking orders and managing reservations.</List.Item>
-                <List.Item>Setting and preparing the room.</List.Item>
-              </List>
-            </div>
-          }
-        />
-        <WorkItem
-          title="Developer Intern"
-          company="iperbooking"
-          link="https://www.iperbooking.com/"
-          date="May 2019 - June 2019"
-          locationlink="https://www.google.com/maps/place/Rimini,+Italy"
-          location="Rimini, RN"
-          description={
-            <div className="justify">
-              <List bulleted>
-                <List.Item>
-                  First steps in the world of web development, learning the
-                  company's workflow.
-                </List.Item>
-                <List.Item>
-                  Website and database management of a Booking Engine.
-                </List.Item>
-              </List>
-            </div>
-          }
-        />
+        {workData.map((workItem, index) => (
+          <WorkItem
+            key={index}
+            title={workItem.title}
+            company={workItem.company}
+            link={workItem.link}
+            date={workItem.date}
+            locationlink={workItem.locationlink}
+            location={workItem.location}
+            description={
+              <div className="justify">
+                <List bulleted>
+                  {workItem.description.map((description, index) => (
+                    <List.Item key={index}>{description}</List.Item>
+                  ))}
+                </List>
+              </div>
+            }
+          />
+        ))}
       </div>
     </div>
   );
