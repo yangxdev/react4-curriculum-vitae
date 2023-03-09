48b9244af892e5af75e5aaf048fc324f7238a50f
yangxdev <j.xiang@campus.unimib.it>
2023-03-09 10:14:29 +0100

    certifications.json

--- src/js/certifications.js
+++ src/js/certifications.js
@@ -1,24 +1,22 @@
 import { List } from "semantic-ui-react";
+import certificationsData from "../json/certifications.json";
 
 function Certifications() {
-    return(
-        <div className="certifications">
-              <div className="title-certifications">CERTIFICATIONS</div>
-              <hr />
-              <div className="certifications-content justify biggerfont">
-                <List bulleted>
-                  <List.Item>
-                    OLS English:
-                    <span style={{ fontWeight: "bold" }}> C1</span>
-                  </List.Item>
-                  <List.Item>
-                    Open Badge Bbetween English:
-                    <span style={{ fontWeight: "bold" }}> C1</span>
-                  </List.Item>
-                </List>
-              </div>
-            </div>
-    );
+  return(
+    <div className="certifications">
+      <div className="title-certifications">CERTIFICATIONS</div>
+      <hr />
+      <div className="certifications-content justify biggerfont">
+        <List bulleted>
+          {certificationsData.map((certification, index) => (
+            <List.Item key={index}>
+              {certification.title}: <span style={{ fontWeight: "bold" }}>{certification.level}</span>
+            </List.Item>
+          ))}
+        </List>
+      </div>
+    </div>
+  );
 }
 
 export default Certifications;
